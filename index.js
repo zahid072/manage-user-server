82b5a677eca22681532e704f8f1dfe2752658567
zahid072 <jahidhasan19368@gmail.com>
2024-04-25 20:15:32 +0600

    user delete method adde

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 const express = require("express");
 const cors = require("cors");
-const app = express()
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const env = require("dotenv").config();
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("manage user server is running...");
+});
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvtbyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get("/", (req, res )=>{
-    res.send("manage user server is running...")
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const usersCollection = client.db("dbUsers2").collection("users2");
 
-app.listen(port, (req, res)=>{
-    console.log(`manage server is running on port: ${port}`)
-})
+    // get users data
+
+    app.get("/users2", async (req, res) => {
+      const cursor = usersCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    // Add users
+    app.post("/users2", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Update users
+
+    // Delete users
+    app.delete("/users2/:id", async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, (req, res) => {
+  console.log(`manage server is running on port: ${port}`);
+});
